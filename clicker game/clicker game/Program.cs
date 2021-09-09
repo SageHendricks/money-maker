c7e045cc62d5e9cf6de42552c803e86c0e34080b
SageHendricks <89399536+SageHendricks@users.noreply.github.com>
2021-09-09 09:40:26 -0400

    Add project files.

---
+++ clicker game/clicker game/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clicker_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clicker();
+
+            Console.ReadKey();
+        }
+
+        static void Clicker(int clicks = 0)
+        {
+            Console.WriteLine("Score: " + clicks);
+            Console.ReadKey();
+            Console.Clear();
+            Clicker(clicks + 1);
+        }
+    }
+}
---
+++ money maker/money maker/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace money_maker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold = 10;
+            int silver = 5;
+
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("how many coins would you like to convert?");
+            double coins = Math.Floor(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine(Convert.ToString(coins) + " cents is equal to...");
+
+            double goldCoins = Math.Floor(coins / gold);
+            double remainder = coins % gold;
+            double silverCoins = Math.Floor(remainder / silver);
+            remainder = remainder % silver;
+
+            Console.WriteLine("Gold Coins: " + Convert.ToString(goldCoins));
+            Console.WriteLine("Silver Coins: " + Convert.ToString(silverCoins));
+            Console.WriteLine("Bronze Coins: " + Convert.ToString(remainder));
+            Console.ReadKey();
+        }
+    }
+}
