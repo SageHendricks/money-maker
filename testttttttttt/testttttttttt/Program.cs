2fd3f53e31650eb44862fe2eda93316f241aabd8
SageHendricks <89399536+SageHendricks@users.noreply.github.com>
2021-10-20 10:36:43 -0400

    fdfffdf

---
+++ testttttttttt/testttttttttt/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace testttttttttt
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Cursor.Hide();
+
+            string[] message = new string[] { "wwwwwwwwwwwwwwwwwwwwwwwwwww", "w                         w", "w                         w", "w                         w", "w                         w", "wwwwwwwwwwwwwwwwwwwwwwwwwww", "" };
+
+            int n = 0;
+
+            int FPS = 60;
+            while (true)
+            {
+                Display.display(String.Join("\n", message));
+                Console.WriteLine(n/FPS);
+                n++;
+                if (n == FPS * 3)
+                {
+                    message[2] = "eeeeeeeeeeeeeeeeeeeeeeeeeee";
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ testttttttttt/testttttttttt/display.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testttttttttt
+{
+    class Display
+    {
+        public static void display(string text, int x = 0, int y = 0)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(text);
+            //WriteAt(message2, x, y);
+        }
+    }
+}
